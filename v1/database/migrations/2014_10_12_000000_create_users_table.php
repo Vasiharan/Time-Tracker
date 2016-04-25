a989061b9e1ab5ca4b19ba6807e4360a7a31a3f6
Vasi <Vasi@Vasiharans-MacBook-Pro.local>
2016-04-25 20:22:15 +0100

    Created Database Migrations for users and time_entries

--- v1/database/migrations/2014_10_12_000000_create_users_table.php
+++ v1/database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,11 +12,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function(Blueprint $table)
+        {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
         });
