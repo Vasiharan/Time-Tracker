6de0827fcd718c5e0a01efe8646abdbeec4ba1c1
Vasi <Vasi@Vasiharans-MacBook-Pro.local>
2016-04-25 21:51:58 +0100

    beautify js code

--- public/scripts/services/user.js
+++ public/scripts/services/user.js
@@ -1,27 +1,27 @@
 (function() {
 
-  'use strict';
+    'use strict';
 
-  angular
-    .module('timeTracker')
-    .factory('user', user);
+    angular
+        .module('timeTracker')
+        .factory('user', user);
 
     function user($resource) {
 
-      // ngResource call to the API for the users
-      var User = $resource('api/users');
+        // ngResource call to the API for the users
+        var User = $resource('api/users');
 
-      // Query the users and return the results
-      function getUsers() {
-        return User.query().$promise.then(function(results) {
-          return results;
-        }, function(error) {
-          console.log(error);
-        });
-      }
+        // Query the users and return the results
+        function getUsers() {
+            return User.query().$promise.then(function(results) {
+                return results;
+            }, function(error) {
+                console.log(error);
+            });
+        }
 
-      return {
-        getUsers: getUsers
-      }
+        return {
+            getUsers: getUsers
+        }
     }
-  })();
+})();
